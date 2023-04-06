b3cb4c32c24fed1efd98ff3372dd2fb1eb35634a
Ikechukwu Nnanta Ananaba <127397185+IkAnanaba@users.noreply.github.com>
2023-04-06 08:34:38 +0100

    cmon man

--- InsuranceClaim.py
+++ InsuranceClaim.py
@@ -4,12 +4,14 @@ from sklearn.preprocessing import StandardScaler
 import pickle
 import streamlit as st
 
-pickles = {'scaler': ['scaler.pkl'], 'encoders': ['encoder.pkl']}
+pickles = {'scaler': ['scaler.pkl'], 'encoders': ['encoders.pkl']}
 model = tf.keras.models.load_model('neural_model')
 
 for pkl in pickles:
     with open(pickles[pkl][0], "rb") as pkl_file:
         pickles[pkl][0] = pickle.load(pkl_file)
 
-print(pickles)
-print("here i am")
+
+
+st.title('See if your building is likely to have an insurance claim')
+

c7a4d505a14458c1896b32c88b6f0054aef0bdab
Ikechukwu Nnanta Ananaba <127397185+IkAnanaba@users.noreply.github.com>
2023-04-12 08:38:07 +0100

    Maybe work

---
+++ neural_model/InsuranceClaimPrediction/InsuranceClaim.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+ 
+import pickle
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+pickles = {'scaler': ['scaler.pkl'], 'encoders': ['encoders.pkl']}
+model = tf.keras.models.load_model('neural_model')
+
+for pkl in pickles:
+    with open(pickles[pkl][0], "rb") as pkl_file:
+        pickles[pkl][0] = pickle.load(pkl_file)
+
+def transform(dataFrame):
+    df = dataFrame.copy()
+    df['Garden'] = df['Garden'].apply(lambda x: int(x))
+    df['Residential'] = df['Residential'].apply(lambda x: int(x))
+    df['Building_Painted'] = df['Building_Painted'].apply(lambda x: int(x))
+    df['Building_Fenced'] = df['Building_Fenced'].apply(lambda x: int(x))
+    df['Settlement'] = df['Settlement'].apply(lambda x: int(x))
+    df['Building_Type'] = df['Building_Type'].apply(lambda x: int(x))
+    
+    return df
+
+
+st.title('Are you liable for an insurance claim?')
+
+YearOfObservation = st.number_input('Year propery was observed',  min_value=2013.00, max_value=2016.00, step=1.00)
+
+YearOfOccupancy = st.number_input('Year of Occupancy',  min_value=2000.00, max_value=2016.00, step=1.00)
+
+InsuredPeriod = st.slider('Buidling insurance period [Full Year = 1, 6 months = 0.5]', max_value=0.00, min_value=1.00, step=0.01)
+
+Residential = st.checkbox('Residential')
+
+BuildingPainted = st.checkbox('Is painted')
+
+BuildingFenced = st.checkbox('Is fenced')
+
+Garden = st.checkbox('Has a Garden')
+
+Settlement = st.checkbox('Settlement')
+
+BuildingDimension = st.number_input('Square Meter area of the building', min_value=1.00, max_value=30745.00, step=1.00)
+
+BuildingType = st.selectbox('Building Type', ['1', '2', '3', '4'])
+
+if st.button('Get your result'):
+    data = {'YearOfObservation': YearOfObservation,
+            'Insured_Period': InsuredPeriod,
+            'Residential': Residential,
+            'Building_Painted': BuildingPainted,
+            'Building_Fenced': BuildingPainted,
+            'Garden' : Garden,
+            'Settlement' : Settlement,
+            'Building Dimension' : BuildingDimension,
+            'Building_Type' : BuildingType,
+            'Date_Of_Occupancy': YearOfOccupancy}
+    data_frame = pd.DataFrame(data, index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    data_frame = transform(data_frame)
+    data = dataframe.values
+    st.write(model.predict(data_frame))
+

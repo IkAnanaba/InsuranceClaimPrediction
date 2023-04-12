d7aec70c39e3c0d41788339e010f9f62abfb03c0
Ikechukwu Nnanta Ananaba <127397185+IkAnanaba@users.noreply.github.com>
2023-04-12 09:05:14 +0100

    testes

--- InsuranceClaim.py
+++ InsuranceClaim.py
@@ -60,7 +60,7 @@ if st.button('Get your result'):
             'Building Dimension' : BuildingDimension,
             'Building_Type' : BuildingType,
             'Date_of_Occupancy': YearOfOccupancy}
-    data_frame = pd.DataFrame(data)
+    data_frame = pd.DataFrame(data, index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
     data_frame = transform(data_frame)
     data_frame = scale(data_frame)
